7f7b2ce6f5b6171d345a8ebf6f5bf050d2c02a22
sahil-nayak <65059575+sahil-nayak@users.noreply.github.com>
2020-08-07 17:49:00 +0530

    Add project files.

---
+++ Controllers/PensionDisbursementController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+//change get to post
+
+namespace PensionDisbursement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PensionDisbursementController : ControllerBase
+    {
+        public IConfiguration configuration;
+        public PensionDisbursementController(IConfiguration iConfig)
+        {
+            configuration = iConfig;
+        }
+        [HttpPost]
+        public int GetDisbursePension(ProcessPensionInput pension)
+        {
+            PensionerDetail pensionerDetail = new PensionerDetail();
+            ServiceWrapper getPensionerDetail = new ServiceWrapper(configuration);
+
+            pensionerDetail = getPensionerDetail.GetDetailResponse(pension.AadharNumber);
+
+
+            if (pensionerDetail == null)
+                return 21;
+
+            int status = 0;
+            int bankServiceCharge;
+            if (pension.BankType == 1)
+                bankServiceCharge = 500;
+            else if (pension.BankType == 2)
+                bankServiceCharge = 550;
+            else
+                bankServiceCharge = 0;
+            double pensionCalculated;
+            pensionCalculated = CalculatePensionLogic(pensionerDetail.SalaryEarned, pensionerDetail.Allowances, bankServiceCharge, pensionerDetail.PensionType);
+
+            if (Convert.ToDouble(pension.PensionAmount) == pensionCalculated)
+            {
+                status = 10;
+            }
+            else
+            {
+                status = 21;
+            }
+            return status;
+            
+        }
+        private double CalculatePensionLogic(int salaryEarned,int allowances,int charge,PensionType type)
+        {
+            if (type == PensionType.Self)
+            {
+                return (salaryEarned * 0.8) + allowances + charge;
+            }
+            else
+            {
+                return (salaryEarned * 0.5) + allowances + charge;
+            }
+
+        }
+        
+
+    }
+}
---
+++ ServiceWrapper.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PensionDisbursement
+{
+    public class ServiceWrapper
+    {
+        private IConfiguration configuration;
+        /// <summary>
+        /// Dependency Injection
+        /// </summary>
+        /// <param name="iConfig"></param>
+        public ServiceWrapper(IConfiguration iConfig)
+        {
+            configuration = iConfig;
+        }
+        /// <summary>
+        /// Getting Value from the Pension Detail Microservice to validate the pension amount
+        /// </summary>
+        /// <param name="aadhar"></param>
+        /// <returns>Status Code</returns>
+        public PensionerDetail GetDetailResponse(string aadhar)
+        {
+           
+            HttpResponseMessage response = new HttpResponseMessage();
+            string uriLink = configuration.GetValue<string>("Disbursementkey:UriLinkValue");
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(uriLink);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                try
+                {
+                    response = client.GetAsync("api/PensionerDetail/" + aadhar).Result;
+                }
+                catch (Exception e) { response = null; }
+            }
+
+            string detailsResponse = response.Content.ReadAsStringAsync().Result;
+            PensionerDetail pen = JsonConvert.DeserializeObject<PensionerDetail>(detailsResponse);
+            
+            return pen;
+        }
+
+       
+    }
+}
